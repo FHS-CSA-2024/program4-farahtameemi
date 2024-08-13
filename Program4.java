46471fd308082fedfde5a7c6fb3d5e99cb6bc874
farahtameemi <irenicgazexx@gmail.com>
2024-08-13 10:07:12 -0700

    Update Program4.java
    Summed four numbers, averaged them, output

--- Program4.java
+++ Program4.java
@@ -1,10 +1,25 @@
 //Youre on your own!
 //Jk ask for help if you need it (but also look at previous programs!!)
-
-
-
+public class Program4 {
+    public static void main(String[] args){
+        //Assign 4 three-digit number to four different variables.
+        int numberOne = 475;
+        int numberTwo = 821;
+        int numberThree = 369;
+        int numberFour= 562;
+        double numberFive = numberOne + numberTwo + numberThree + numberFour;
+        double numberSix = numberFive/4;
+        //Then have the computer sum them and store the sum in a fifth variable.
+        System.out.println("The sum of the four numbers is " + numberFive);
+        System.out.println();
+        //Compute the average and store the answer in a sixth variable.
+        System.out.println("The average of the four numbers is " + numberSix);
+    }
+}
 //Paste console output below:
 /*
+The sum of the four numbers is 2227.0
 
+The average of the four numbers is 556.75
 
 */
